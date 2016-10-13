77d7d720c82e3e18754bf53e63f3282ba970db2e
liverbool <nukboon@gmail.com>
2016-10-14 03:33:45 +0700

    bugfix

--- Gulpfile.js
+++ Gulpfile.js
@@ -22,9 +22,6 @@ var paths = {
             nodeRoot + 'codemirror/mode/yaml/yaml.js',
             nodeRoot + 'codemirror/mode/css/css.js',
             nodeRoot + 'codemirror/mode/javascript/javascript.js',
-            '../../../vendor/toro/widget-bundle/Resources/public/js/waypoints.min.js',
-            '../../../vendor/toro/widget-bundle/Resources/public/js/inview.min.js',
-            '../../../vendor/toro/widget-bundle/Resources/public/js/widget.js',
             'Resources/private/js/**'
         ],
         sass: [
@@ -34,8 +31,7 @@ var paths = {
             'Resources/private/img/**'
         ],
         css: [
-            nodeRoot + 'codemirror/lib/codemirror.css',
-            '../../../vendor/toro/widget-bundle/Resources/public/css/style.css'
+            nodeRoot + 'codemirror/lib/codemirror.css'
         ]
     }
 };
