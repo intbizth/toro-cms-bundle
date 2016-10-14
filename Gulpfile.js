8f9c6503840e6e02cc71208d901933014d0c35c5
liverbool <nukboon@gmail.com>
2016-10-14 18:57:39 +0700

    update assets build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,64 +1,64 @@
-var concat = require('gulp-concat');
-var env = process.env.GULP_ENV;
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
-var livereload = require('gulp-livereload');
-var merge = require('merge-stream');
-var order = require('gulp-order');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var debug = require('gulp-debug');
+var order = require('gulp-order');
+var merge = require('merge-stream');
+var gutil = require('gulp-util');
+
+var env = gutil.env.env;
+var stage = gutil.env.stage;
 
 var rootPath = '../../../web/assets/cms/';
-var nodeRoot = '../../../node_modules/';
+var nodePath = '../../../node_modules/';
 
-if ('dev' === env) {
+if ('dev' === stage) {
     rootPath = "../" + rootPath;
     nodePath = "../" + nodePath;
 }
 
 var paths = {
-    cms: {
-        js: [
-            nodeRoot + 'codemirror/lib/codemirror.js',
-            nodeRoot + 'codemirror/mode/twig/twig.js',
-            nodeRoot + 'codemirror/mode/xml/xml.js',
-            nodeRoot + 'codemirror/mode/yaml/yaml.js',
-            nodeRoot + 'codemirror/mode/css/css.js',
-            nodeRoot + 'codemirror/mode/javascript/javascript.js',
-            'Resources/private/js/**'
-        ],
-        sass: [
-            'Resources/private/sass/**'
-        ],
-        img: [
-            'Resources/private/img/**'
-        ],
-        css: [
-            nodeRoot + 'codemirror/lib/codemirror.css'
-        ]
-    }
+    js: [
+        nodePath + 'codemirror/lib/codemirror.js',
+        nodePath + 'codemirror/mode/twig/twig.js',
+        nodePath + 'codemirror/mode/xml/xml.js',
+        nodePath + 'codemirror/mode/yaml/yaml.js',
+        nodePath + 'codemirror/mode/css/css.js',
+        nodePath + 'codemirror/mode/javascript/javascript.js',
+        'Resources/private/js/**'
+    ],
+    sass: [
+        'Resources/private/sass/**'
+    ],
+    css: [
+        nodePath + 'codemirror/lib/codemirror.css'
+    ],
+    copy: [
+        ['images', 'Resources/private/images/**']
+    ]
 };
 
-gulp.task('cms-js', function () {
-    return gulp.src(paths.cms.js)
+gulp.task('script', function () {
+    return gulp.src(paths.js)
         .pipe(concat('app.js'))
         .pipe(gulpif(env === 'prod', uglify()))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(rootPath + 'js/'))
-    ;
+        .pipe(gulp.dest(rootPath + 'js/'));
 });
 
-gulp.task('cms-css', function() {
-    var cssStream = gulp.src(paths.cms.css)
-        .pipe(concat('css-files.css'))
-    ;
+gulp.task('style', function () {
+    var cssStream = gulp.src(paths.css)
+            .pipe(concat('css-files.css'))
+        ;
 
-    var sassStream = gulp.src(paths.cms.sass)
-        .pipe(sass())
-        .pipe(concat('sass-files.scss'))
-    ;
+    var sassStream = gulp.src(paths.sass)
+            .pipe(sass())
+            .pipe(concat('sass-files.scss'))
+        ;
 
     return merge(cssStream, sassStream)
         .pipe(order(['css-files.css', 'sass-files.scss']))
@@ -66,25 +66,20 @@ gulp.task('cms-css', function() {
         .pipe(gulpif(env === 'prod', uglifycss()))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(rootPath + 'css/'))
-        .pipe(livereload())
-    ;
+        ;
 });
 
-gulp.task('cms-img', function() {
-    return gulp.src(paths.cms.img)
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(rootPath + 'img/'))
-    ;
+gulp.task('copy', function () {
+    for (var i = 0; i < paths.copy.length; i++) {
+        var copy = paths.copy[i];
+        gulp.src(copy[1]).pipe(gulp.dest(rootPath + copy[0]));
+    }
 });
 
-gulp.task('cms-watch', function() {
-    livereload.listen();
-
-    gulp.watch(paths.cms.js, ['cms-js']);
-    gulp.watch(paths.cms.sass, ['cms-css']);
-    gulp.watch(paths.cms.css, ['cms-css']);
-    gulp.watch(paths.cms.img, ['cms-img']);
+gulp.task('watch', function () {
+    gulp.watch(paths.js, ['script']);
+    gulp.watch(paths.sass, ['style']);
+    gulp.watch(paths.css, ['style']);
 });
 
-gulp.task('default', ['cms-js', 'cms-css', 'cms-img']);
-gulp.task('watch', ['default', 'cms-watch']);
+gulp.task('default', ['script', 'style', 'copy']);
