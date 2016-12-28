458707e5841c9f689d3467afa804b0134caefa6f
liverbool <nukboon@gmail.com>
2016-12-29 05:19:11 +0700

    bugfix

--- Doctrine/ORM/PostRepository.php
+++ Doctrine/ORM/PostRepository.php
@@ -4,5 +4,17 @@ namespace Toro\Bundle\CmsBundle\Doctrine\ORM;
 
 class PostRepository extends PageRepository
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function findPageForDisplay(array $criteria)
+    {
+        $criteria['published'] = true;
 
+        if (array_key_exists('partial', $criteria)) {
+            unset($criteria['partial']);
+        }
+
+        return parent::findPageForDisplay($criteria);
+    }
 }
