b15f6567bfc9c9a44888a05f0c70b38835dae75b
liverbool <nukboon@gmail.com>
2017-01-06 02:27:23 +0700

    add useful feature

---
+++ Twig/PrintifExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Toro\Bundle\CmsBundle\Twig;
+
+class PrintifExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('printif', array($this, 'printif')),
+        );
+    }
+
+    /**
+     * @param string $text
+     * @param bool $condition
+     *
+     * @return string
+     */
+    public function printif($text, $condition)
+    {
+        return $condition ? $text : '';
+    }
+}
