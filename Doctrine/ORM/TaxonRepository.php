34560f5af11aace689b69af1cbd87844bd08073c
liverbool <nukboon@gmail.com>
2017-03-06 01:17:37 +0700

    bugfix

--- Doctrine/ORM/TaxonRepository.php
+++ Doctrine/ORM/TaxonRepository.php
@@ -6,47 +6,4 @@ use Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository as BaseTaxonReposi
 
 class TaxonRepository extends BaseTaxonRepository
 {
-    /**
-     * {@override}
-     */
-    public function findChildren($taxonCode)
-    {
-        if (!$taxon = $this->findOneBy(['code' => $taxonCode])) {
-            return [];
-        }
-
-        $root = $taxon->isRoot() ? $taxon : $taxon->getRoot();
-        $queryBuilder = $this->createQueryBuilder('o');
-
-        $queryBuilder
-            ->andWhere($queryBuilder->expr()->eq('o.root', ':root'))
-            ->andWhere($queryBuilder->expr()->gt('o.left', ':left'))
-            ->andWhere($queryBuilder->expr()->lt('o.right', ':right'))
-            ->setParameter('root', $root)
-            ->setParameter('left', $taxon->getLeft())
-            ->setParameter('right', $taxon->getRight())
-
-            ->orderBy('o.left', 'ASC')
-        ;
-
-        return $queryBuilder->getQuery()->getResult();
-    }
-
-    /**
-     * {@override}
-     * @fixme find in used and try to fixme
-     */
-    public function findNodesTreeSorted($rootCode = NULL)
-    {
-        $taxons = [];
-        $roots = $this->findRootNodes();
-
-        foreach ($this->findRootNodes() as $root) {
-            $taxons[] = $root;
-
-            $taxons = array_merge($taxons, $this->findChildren($root->getCode()));
-        }
-
-        return $taxons;
-    }
 }
