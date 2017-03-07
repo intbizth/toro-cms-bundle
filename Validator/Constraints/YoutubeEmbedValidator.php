32dfa0e53bc7f949b69dd428948d468c7e04d13a
liverbool <nukboon@gmail.com>
2017-03-08 05:44:35 +0700

    bugfix

--- Validator/Constraints/YoutubeEmbedValidator.php
+++ Validator/Constraints/YoutubeEmbedValidator.php
@@ -11,7 +11,11 @@ class YoutubeEmbedValidator extends ConstraintValidator
      * {@inheritdoc}
      */
     public function validate($value, Constraint $constraint)
-    {dump($value);
+    {
+        if (!$value) {
+            return;
+        }
+
         if (!preg_match('%^(?:https?://)?(?:www\.)?(?:youtu\.be/| youtube\.com(?:/embed/))([\w-]{10,12})$%x', $value)) {
             $this->context->buildViolation($constraint->message)
                 ->setParameter('%string%', $value)
