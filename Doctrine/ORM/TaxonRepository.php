15d4be200a114a1f1ef0a4842aa9e6b20d557aff
liverbool <nukboon@gmail.com>
2017-03-07 18:21:40 +0700

    fixbug

--- Doctrine/ORM/TaxonRepository.php
+++ Doctrine/ORM/TaxonRepository.php
@@ -6,4 +6,26 @@ use Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository as BaseTaxonReposi
 
 class TaxonRepository extends BaseTaxonRepository
 {
+    /**
+     * @bugfix https://github.com/Sylius/Sylius/pull/7437
+     *
+     * {@inheritdoc}
+     */
+    public function findNodesTreeSorted($rootCode = null)
+    {
+        $queryBuilder = $this->createQueryBuilder('o')
+            ->addOrderBy('o.root')
+            ->addOrderBy('o.left')
+        ;
+
+        if (null !== $rootCode) {
+            $queryBuilder
+                ->join('o.root', 'root')
+                ->andWhere('root.code = :rootCode')
+                ->setParameter('rootCode', $rootCode)
+            ;
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
 }
