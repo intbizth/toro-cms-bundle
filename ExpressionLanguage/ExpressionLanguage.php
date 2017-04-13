f24050cba6832de74c6c2328d99674f1e55bac6c
liverbool <nukboon@gmail.com>
2017-04-14 02:03:46 +0700

    add ExpressionLanguage

---
+++ ExpressionLanguage/ExpressionLanguage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Toro\Bundle\CmsBundle\ExpressionLanguage;
+
+use Symfony\Component\DependencyInjection\ExpressionLanguage as BaseExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ParserCache\ParserCacheInterface;
+
+final class ExpressionLanguage extends BaseExpressionLanguage
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(ParserCacheInterface $parser = null, array $providers = array())
+    {
+        array_unshift($providers, new DefaultNullValueExpressionFunctionProvider());
+
+        parent::__construct($parser, $providers);
+    }
+}
