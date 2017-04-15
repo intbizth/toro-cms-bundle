7c039c3c55df3166cd1015eefd2f8e0b7dd2ecf9
liverbool <nukboon@gmail.com>
2017-04-15 13:09:54 +0700

    fix bug

--- ExpressionLanguage/ExpressionLanguage.php
+++ ExpressionLanguage/ExpressionLanguage.php
@@ -2,6 +2,7 @@
 
 namespace Toro\Bundle\CmsBundle\ExpressionLanguage;
 
+use Sylius\Bundle\ResourceBundle\ExpressionLanguage\NotNullExpressionFunctionProvider;
 use Symfony\Component\DependencyInjection\ExpressionLanguage as BaseExpressionLanguage;
 use Symfony\Component\ExpressionLanguage\ParserCache\ParserCacheInterface;
 
@@ -12,6 +13,7 @@ final class ExpressionLanguage extends BaseExpressionLanguage
      */
     public function __construct(ParserCacheInterface $parser = null, array $providers = array())
     {
+        array_unshift($providers, new NotNullExpressionFunctionProvider());
         array_unshift($providers, new DefaultNullValueExpressionFunctionProvider());
 
         parent::__construct($parser, $providers);
